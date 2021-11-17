14ea89b04ba29d5ea27082d022e4d07d562a9f8f
addiegale <78458107+addiegale@users.noreply.github.com>
2021-11-16 19:36:33 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,23 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate = video.playbackRate * .95;
+	console.log("The Speed is " + video.playbackRate)
+});
+
